2b45716ae2e0781b77ba0e053b3f493cd86e963d
Andres Cupa <jamendoza23@misena.edu.co>
2020-10-12 19:45:32 -0500

    metodos

--- resources/assets/js/components/Cliente.vue
+++ resources/assets/js/components/Cliente.vue
@@ -68,3 +68,115 @@
         </div>
     </main>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            arrayDatos:[],
+            nombre:"",
+            idCat:0,
+            modal:0,
+            accion:0,
+            titulo:"",
+            buscar:"",
+            criterio:"nombre",
+        }
+    },
+    methods:{
+        listCat(){
+           let me = this;
+           var url ="/categoria";
+           axios.get(url).then(function(response){
+               var respuesta = response.data;
+               me.arrayDatos = respuesta.categorias.data;
+           })
+           .catch(function(error){
+               console.log(error)
+           })
+        },
+        regCat(){
+           let me = this;
+           var url ="/categoria/registrar";
+           axios.post(url,{
+               nombre:this.nombre
+           })
+           .then(function(response){
+               alert('se gurado correctamente');
+               me.listCat();
+           })
+           .catch(function(error){
+               console.log(error)
+           })
+        },
+        actCat(){
+           let me = this;
+           var url ="/categoria/actualizar";
+           axios.put(url,{
+               id:this.idCat,
+               nombre:this.nombre
+           })
+           .then(function(response){
+               alert('se actualizo correctamente');
+               me.listCat();
+               me.cerrarModal();
+           })
+           .catch(function(error){
+               console.log(error)
+           })
+        },
+        eliminaCat(data=[]){
+           let me = this;
+           var url ="/categoria/actualizar";
+           axios.post(url,{
+               id:data['id']
+           })
+           .then(function(response){
+               alert('se elimino correctamente');
+               me.listCat();   
+           })
+           .catch(function(error){
+               console.log(error)
+           })
+        },
+        abrirModal(accion,data=[]){
+            switch (accion) {
+                case 'guardar':
+                    this.titulo = 'se guardo';
+                    this.accion = 0;
+                    this.limpiar();
+                    break;
+                case 'editar':
+                    this.titulo = 'se edito';
+                    this.accion = 1;
+                    this.idCat = data['id'];
+                    this.nombre = data['nombre'];
+                    break;
+                default:
+                    break;
+            }
+            this.modal=1;
+        },
+        cerrarModal(){
+            this.modal = 0;
+        },
+        limpiar(){
+            this.nombre = "";
+        }
+    },
+    mounted(){
+        console.log('component mounted.');
+        this.listCat(1,this.criterio,this.buscar);
+    }
+}
+</script>
+<style>
+    .modal-content{
+       width: 100% !important;
+       position: absolute !important;
+    }
+    .mostrar{
+       display: list-item !important;
+       opacity: 1 !important;
+       position: absolute !important;
+    }
+</style>
